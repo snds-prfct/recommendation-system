5a2be185d15aeb46f012fafe7a16c50295a0afa9
snds_prfct <snds.prfct@gmail.com>
2025-11-03 17:14:30 +0300

    Recommendation Service small update and refactoring. Tests

---
+++ recommendation-service/src/test/java/dev/snds_prfct/recommendations/RecommendationsApplicationTest.java
@@ -0,0 +1,11 @@
+package dev.snds_prfct.recommendations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class RecommendationsApplicationTest {
+
+    @Test
+    void testAppStarts() {}
+}
