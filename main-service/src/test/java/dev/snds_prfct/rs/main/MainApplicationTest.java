6eff7ff5bd28ff035b89efd2c2205b732c86c782
snds_prfct <snds.prfct@gmail.com>
2026-03-01 09:24:07 +0300

    Version 0.1. Java 25. Spring Boot 4.0.3

--- main-service/src/test/java/dev/snds_prfct/rs/main/MainApplicationTest.java
+++ main-service/src/test/java/dev/snds_prfct/rs/main/MainApplicationTest.java
@@ -2,8 +2,10 @@ package dev.snds_prfct.rs.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1, topics = "kafka.topics.users-activity.topic")
 class MainApplicationTest {
 
     @Test
